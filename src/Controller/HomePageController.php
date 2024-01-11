e4d8d858771e9e59492c869186129cc22f930589
MelanyFroment <84186028+MelanyFroment@users.noreply.github.com>
2024-01-11 14:53:08 +0100

    form et affiche

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +13,12 @@ class HomePageController extends AbstractController
      * @Route("/", name="app_home_page")
      */
 
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+        $products = $productRepository->findAll();
         return $this->render('base.html.twig', [
             'controller_name' => 'HomePageController',
+            'products' => $products,
         ]); 
     }
 }
--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Form\ProductFormType;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,15 +14,36 @@ class ProductsController extends AbstractController
     /**
      * @Route("/products", name="app_products_index")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
 
         $product = new Product();
 
         $form = $this->createForm(ProductFormType::class, $product);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Obtenez l'EntityManager
+            $entityManager = $this->getDoctrine()->getManager();
+
+            // Persistez l'entité Product
+            $entityManager->persist($product);
+
+            // Flush pour sauvegarder en base de données
+            $entityManager->flush();
+
+            // Ajoutez un message flash pour indiquer le succès
+            $this->addFlash('success', 'Produit créer avec succès.');
+
+            // Redirigez vers une autre page ou effectuez toute autre action nécessaire
+            return $this->redirectToRoute('app_products_index');
+        }
+
+
+
         return $this->render('products/index.html.twig', [
             'formProduct' => $form->createView(),
         ]);
-    }
+    } 
 }
